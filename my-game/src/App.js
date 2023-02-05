fa2868dfe5b490aeef2da8d6ab19390774c3de46
CJskii <czareczarek3@gmail.com>
2023-02-05 21:43:32 +0000

    Initial commit

---
+++ my-game/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app bg-slate-600 h-screen">
+      {/* HEADER
+      SCORE
+      BOARD
+      FOOTER */}
+      <h1 className="font-bold text-center p-4 text-5xl text-blue-600">
+        Memory Card Game
+      </h1>
+    </div>
+  );
+};
+
+export default App;
